792b687651b79cc174d8339c43c6163cac3c4e4c
town174 <351193314@qq.com>
2016-10-18 08:05:14 +0800

    新增用户管理对应的BLL,DAL,Utils

---
+++ code/hxgUtilis/DataConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Xml;
+
+namespace hxgUtilis
+{
+    public static class DataConverter
+    {
+        public static DataTable ConvertToDataTable<T>(this IEnumerable<T> enumerable)
+        {
+            
+            var dataTable = new DataTable();
+            foreach (PropertyDescriptor pd in TypeDescriptor.GetProperties(typeof(T)))
+            {
+                dataTable.Columns.Add(pd.Name);//加上pd.PropertyType会报错
+            }
+            foreach (T item in enumerable)
+            {
+                var Row = dataTable.NewRow();
+
+                foreach (PropertyDescriptor dp in TypeDescriptor.GetProperties(typeof(T)))
+                {
+                    Row[dp.Name] = dp.GetValue(item);
+                }
+                dataTable.Rows.Add(Row);
+            }
+            return dataTable;
+        }        
+    }
+}
